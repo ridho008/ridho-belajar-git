be67174703d2c97082f5a7806f77f990f99f8f72
Ridho Surya <ciuyy12@gmail.com>
2020-04-29 20:08:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 </head>
 <body>
 <h1>Hello Dunia</h1>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta ipsa laudantium suscipit quod, eius reiciendis, cum corporis cupiditate. Nostrum quisquam libero alias nam nulla a sequi, debitis excepturi eaque tempore repellendus, eum fugiat dolorem blanditiis fuga laudantium ut labore, maxime voluptate facilis natus pariatur. Soluta laboriosam similique libero, magnam animi impedit maiores sit possimus. Distinctio dolorum repudiandae ducimus quibusdam perspiciatis esse, molestias ea enim! Amet dolore suscipit ex sunt in repellat asperiores numquam, iste fugit, saepe ipsa dolor adipisci facilis sapiente fugiat accusantium delectus quos quod veritatis, quas nobis eos nemo iure rem. Quo excepturi magnam voluptatum quisquam, tempora beatae.	
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta ipsa laudantium suscipit quod, eius reiciendis, cum corporis cupiditate. Nostrum quisquam libero alias nam nulla a sequi, debitis excepturi eaque tempore repellendus, eum fugiat dolorem blanditiis fuga laudantium ut labore, maxime voluptate facilis natus pariatur. Soluta laboriosam similique libero, magnam animi impedit maiores sit possimus. Distinctio dolorum repudiandae ducimus quibusdam perspiciatis esse, molestias ea enim! Amet dolore suscipit ex sunt in repellat asperiores numquam, iste fugit, saepe ipsa dolor adipisci facilis sapiente fugiat accusantium delectus quos quod veritatis, quas nobis eos nemo iure rem. Quo excepturi magnam voluptatum quisquam, tempora beatae.
+
+<ul>
+	<li>Ridho Surya</li>
+</ul>	
 </body>
 </html>
